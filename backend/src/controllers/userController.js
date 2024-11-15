1d207591aef7422a19cb7cdcb0c67a2907b9f89d
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-15 21:05:41 +0200

    create user contoller

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,25 @@
+import authService from '../services/authService.js';
+
+const register = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const newUser = await authService.registerUser(username, email, password);
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const token = await authService.loginUser(email, password);
+        res.json({ token });
+    } catch (error) {
+        res.status(401).json({ message: error.message });
+    }
+};
+
+export default { register, login };
