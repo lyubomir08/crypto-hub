0dc18d550728774ea1ee4eb5f1837e8a5c6f5c71
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-17 14:36:45 +0200

    register about comp in the routes

--- frontend/crypto-hub/src/app/app.routes.ts
+++ frontend/crypto-hub/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
     {path: '404', component: ErrorComponent},
     {path: '**', redirectTo: '/404', pathMatch: 'full'},
 ];
