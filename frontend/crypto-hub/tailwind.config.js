52d5c260e6993d69d402a8d48bec031449c6b998
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-17 13:40:30 +0200

    config tailwindcss

---
+++ frontend/crypto-hub/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Include all Angular component files and HTML files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
