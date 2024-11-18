584f343af38053a8c025e01f89509a4b8eda464b
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-18 19:06:42 +0200

    config login and register routes

--- frontend/crypto-hub/src/app/app.routes.ts
+++ frontend/crypto-hub/src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'about', component: AboutComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
     {path: '404', component: ErrorComponent},
     {path: '**', redirectTo: '/404', pathMatch: 'full'},
 ];
