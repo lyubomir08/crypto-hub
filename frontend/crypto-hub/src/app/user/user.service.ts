8bfb1cdb6beb24d2089a8bf93e2fc08ef8caed86
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-19 16:36:20 +0200

    make fake user auth

---
+++ frontend/crypto-hub/src/app/user/user.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class UserService {
+    USER_KEY = '[user]';
+    user: object | null = null;
+
+    get isLogged(): boolean {
+        return !!this.user;
+    }
+
+    constructor() {
+        try {
+            const lsUser = localStorage.getItem(this.USER_KEY) || '';
+            this.user = JSON.parse(lsUser);
+        } catch (error) {
+            this.user = null;
+        }
+    }
+
+    login() {
+        this.user = {
+            firstName: 'John',
+            email: 'john.doe@abv.bg',
+            phoneNumber: '123-123-213',
+            password: '123123',
+            id: 'asdasdsadsadsa',
+        };
+
+        localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+    }
+
+    logout() {
+        this.user = null;
+        localStorage.removeItem(this.USER_KEY);
+    }
+}
