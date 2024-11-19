a55c7c108762b2fd6e6bd31ff6c8d493eb265ccc
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-19 15:51:59 +0200

    add routes

--- frontend/crypto-hub/src/app/app.routes.ts
+++ frontend/crypto-hub/src/app/app.routes.ts
@@ -4,11 +4,16 @@ import { ErrorComponent } from './error/error.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
+import { CatalogComponent } from './cryptos/catalog/catalog.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'about', component: AboutComponent},
+    { path: 'catalog', children: [
+        {path: '', component: CatalogComponent},
+        {}
+    ] }
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: '404', component: ErrorComponent},
