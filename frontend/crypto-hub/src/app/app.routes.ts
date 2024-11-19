6c32bd91b3129f5cb1fe864c6dc8925c08cf25cd
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-19 16:31:24 +0200

    update path and route

--- frontend/crypto-hub/src/app/app.routes.ts
+++ frontend/crypto-hub/src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'about', component: AboutComponent},
-    { path: 'catalog', children: [
+    { path: 'cryptos', children: [
         {path: '', component: CatalogComponent},
     ] },
     {path: 'login', component: LoginComponent},
