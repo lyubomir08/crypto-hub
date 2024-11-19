43101c6cb0c227e8f2447952e34d06d5fc2d13d9
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-19 16:28:59 +0200

    add api call to get all cryptos

--- frontend/crypto-hub/src/app/cryptos/catalog/catalog.component.ts
+++ frontend/crypto-hub/src/app/cryptos/catalog/catalog.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { Crypto } from '../../types/crypto';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
   imports: [],
   templateUrl: './catalog.component.html',
-  styleUrl: './catalog.component.css'
+  styleUrl: './catalog.component.css',
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+  cryptos: Crypto[] = [];
 
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.getCryptos().subscribe((c) => {
+      this.cryptos = c;
+    });
+  };
 }
