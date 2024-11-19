3fb58153e977491098fb41e0ee10dcc134c7e5f5
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-19 16:22:01 +0200

    add type

--- frontend/crypto-hub/src/app/api.service.ts
+++ frontend/crypto-hub/src/app/api.service.ts
@@ -1,15 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Crypto } from './types/crypto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  baseUrl = `http://localhost:5000/api/cryptos`;
+  private baseUrl = `http://localhost:5000/api/cryptos`;
 
   constructor(private http: HttpClient) { }
 
   getCryptos() {
-    return this.http.get(this.baseUrl);
+    return this.http.get<Crypto[]>(this.baseUrl);
   }
 }
