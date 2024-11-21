a8b77f15cf4e4556f7afc5ed2aa0e3ed6878fb6e
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-21 11:00:31 +0200

    config auth middleware

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const authMiddleware = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Authentication token missing' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.userId;
+
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid or expired token' });
+    }
+};
+
+export default authMiddleware;
