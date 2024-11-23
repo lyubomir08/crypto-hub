16503a32e7e69cdd97df968803d9903a891f267d
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-23 17:58:07 +0200

    make directive configs

--- frontend/crypto-hub/src/app/directives/email.directive.ts
+++ frontend/crypto-hub/src/app/directives/email.directive.ts
@@ -1,11 +1,20 @@
 import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
   selector: '[appEmail]',
-  standalone: true
+  standalone: true,
+  providers: [{
+    provide: NG_VALIDATORS,
+    multi: true,
+    useExisting: EmailDirective,
+  }]
 })
-export class EmailDirective {
+export class EmailDirective implements Validator {
 
   constructor() { }
 
+  validate(control: AbstractControl): ValidationErrors | null {
+    return null;
+  }
 }
