4a9801f36e7075f4b0a84940dff549f695cc655a
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-24 14:00:10 +0200

    add getters

--- frontend/crypto-hub/src/app/user/register/register.component.ts
+++ frontend/crypto-hub/src/app/user/register/register.component.ts
@@ -10,15 +10,29 @@ import { RouterLink } from '@angular/router';
     styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-    form =  new FormGroup({
-        username: new FormControl('', [Validators.required]),
+    form = new FormGroup({
+        username: new FormControl('', [Validators.required, Validators.minLength(5),]),
         email: new FormControl('', [Validators.required]),
         password: new FormControl('', [Validators.required]),
         rePassword: new FormControl('', [Validators.required]),
     });
 
+    get isUsernameMissing() {
+        return (
+            this.form.get('username')?.touched &&
+            this.form.get('username')?.errors?.['required']
+        );
+    }
+
+    get isNotMinLength() {
+        return (
+            this.form.get('username')?.touched &&
+            this.form.get('username')?.errors?.['minlength']
+        );
+    }
+
     register() {
-        if(this.form.invalid) {
+        if (this.form.invalid) {
             return;
         }
 
