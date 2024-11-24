b8c95288245a1f52e55c6225d9c5464855f53ce0
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-24 15:43:37 +0200

    create add-crypto component

---
+++ frontend/crypto-hub/src/app/cryptos/add-crypto/add-crypto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-crypto',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-crypto.component.html',
+  styleUrl: './add-crypto.component.css'
+})
+export class AddCryptoComponent {
+
+}
