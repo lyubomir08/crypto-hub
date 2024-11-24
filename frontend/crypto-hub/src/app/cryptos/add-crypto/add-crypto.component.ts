2115b4fbd2b2d860d413cb249d4f2808a6d9a6b0
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-24 16:47:03 +0200

    add create func

--- frontend/crypto-hub/src/app/cryptos/add-crypto/add-crypto.component.ts
+++ frontend/crypto-hub/src/app/cryptos/add-crypto/add-crypto.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-add-crypto',
-  standalone: true,
-  imports: [FormsModule],
-  templateUrl: './add-crypto.component.html',
-  styleUrl: './add-crypto.component.css'
+    selector: 'app-add-crypto',
+    standalone: true,
+    imports: [FormsModule],
+    templateUrl: './add-crypto.component.html',
+    styleUrl: './add-crypto.component.css'
 })
 export class AddCryptoComponent {
 
-  create() {}
+    create(form: NgForm) {
+        if (form.invalid) {
+            console.error("Invalid create form");
+            return;
+        }
+        
+    }
 }
