042a070fe1cbb6fff0e401f7a43156ccf6590dec
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-24 13:25:40 +0200

    add basic needs

--- frontend/crypto-hub/src/app/user/register/register.component.ts
+++ frontend/crypto-hub/src/app/user/register/register.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-register',
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+    selector: 'app-register',
+    standalone: true,
+    imports: [RouterLink, ReactiveFormsModule],
+    templateUrl: './register.component.html',
+    styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+    form =  new FormGroup({
+        username: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required]),
+        rePassword: new FormControl('', [Validators.required]),
+    });
 
+    register() {
+        console.log(this.form.invalid);
+    }
 }
