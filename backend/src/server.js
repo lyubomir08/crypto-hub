d4bd8410c3d98c49615596594e27de6db9ec8d78
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-29 10:27:58 +0200

    config libraries

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import userController from './controllers/userController.js';
 import cryptoController from './controllers/cryptoController.js';
 import authMiddleware from './middlewares/authMiddleware.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/cryptoApp'
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.post('/api/users/register', userController.register);
 app.post('/api/users/login', userController.login);
