991ad0cc8a5f69fd07b6fb23af32fcf4237eb447
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-30 12:19:55 +0200

    update auth middleware

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,18 +1,26 @@
+import jwt from "../utils/jwt.js";
+
 const authMiddleware = async (req, res, next) => {
-    const token = req.cookies['auth'];
-
-    if (!token) {
-        return res.status(401).json({ message: 'Not authenticated' });
-    }
-
     try {
+        const token = req.cookies['auth'];
+
+        if (!token) {
+            return res.status(401).json({ message: 'Not authenticated. Token is missing.' });
+        }
+
         const decoded = await jwt.verify(token, process.env.JWT_SECRET);
         req.userId = decoded.userId;
         req.isAuthenticated = true;
 
         next();
     } catch (error) {
-        res.status(401).json({ message: 'Invalid or expired token' });
+        if (error.name === 'JsonWebTokenError') {
+            return res.status(401).json({ message: 'Invalid token' });
+        } else if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ message: 'Token has expired' });
+        } else {
+            return res.status(500).json({ message: 'Internal server error' });
+        }
     }
 };
 
