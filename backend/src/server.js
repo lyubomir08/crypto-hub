eb9ad085bfb2c06ca4dfdecbf9477d59c205324b
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-30 11:33:40 +0200

    config cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/cryptoApp';
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
