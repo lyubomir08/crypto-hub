cf1406a9febf31981a4d3669478da4fa127d6af6
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-30 10:35:14 +0200

    config interceptor

--- frontend/crypto-hub/src/app/app.interceptor.ts
+++ frontend/crypto-hub/src/app/app.interceptor.ts
@@ -1,5 +1,17 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from '../environments/environment.development';
+
+const { apiUrl } = environment;
+const API = '/api';
 
 export const appInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+    if(req.url.startsWith(API)) {
+        req = req.clone({
+            url: req.url.replace(API, apiUrl),
+            // withCredentials: true,
+        })
+    }
+    
+
+    return next(req);
 };
