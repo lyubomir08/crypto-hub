73ed08cada6680ae0faceb3e6b693e6c99db8386
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-11-30 12:19:47 +0200

    add auth middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,8 @@ app.use(cookieParser());
 app.post('/api/users/register', userController.register);
 app.post('/api/users/login', userController.login);
 app.get('/api/users/logout', userController.logout);
-app.get('/api/users/profile', userController.getProfileInfo);
+
+app.get('/api/users/profile',  authMiddleware, userController.getProfileInfo);
 
 app.get('/api/cryptos', cryptoController.getAllCryptos);
 app.get('/api/cryptos/:id/details', cryptoController.getCryptoById);
