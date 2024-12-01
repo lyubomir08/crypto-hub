55a0bc09dd113c144494b2457c46a1bcfd912425
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-01 13:55:50 +0200

    create error msg component

---
+++ frontend/crypto-hub/src/app/core/error-msg/error-msg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-msg',
+  standalone: true,
+  imports: [],
+  templateUrl: './error-msg.component.html',
+  styleUrl: './error-msg.component.css'
+})
+export class ErrorMsgComponent {
+
+}
