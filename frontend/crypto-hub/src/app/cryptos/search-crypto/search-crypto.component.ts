bee68b4306d329cbe432a45975348c055d215194
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-01 15:14:29 +0200

    create search component

---
+++ frontend/crypto-hub/src/app/cryptos/search-crypto/search-crypto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-crypto',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-crypto.component.html',
+  styleUrl: './search-crypto.component.css'
+})
+export class SearchCryptoComponent {
+
+}
