9be650fdfb929c3982751cd331ceb3c6160c5689
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-01 16:17:50 +0200

    add on init vizualization

--- frontend/crypto-hub/src/app/cryptos/search-crypto/search-crypto.component.ts
+++ frontend/crypto-hub/src/app/cryptos/search-crypto/search-crypto.component.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ApiService } from '../../api.service';
 import { Crypto } from '../../types/crypto';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from '../../shared/loader/loader.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-search-crypto',
     standalone: true,
-    imports: [FormsModule, CommonModule, LoaderComponent],
+    imports: [FormsModule, CommonModule, LoaderComponent, RouterLink],
     templateUrl: './search-crypto.component.html',
     styleUrls: ['./search-crypto.component.css'],
 })
-export class SearchCryptoComponent {
+export class SearchCryptoComponent implements OnInit {
     cryptos: Crypto[] = [];
     isLoading = false;
 
     constructor(private apiService: ApiService) { }
+
+    ngOnInit(): void {
+        this.apiService.getCryptos().subscribe((data) => {
+            this.cryptos = data;
+        });
+    }
 
     search(form: NgForm) {
         if(form.invalid) {
