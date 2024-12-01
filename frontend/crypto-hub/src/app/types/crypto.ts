0f97a7cef0c401fa00f0b6e94e91619f19038c29
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-01 19:39:47 +0200

    create type

--- frontend/crypto-hub/src/app/types/crypto.ts
+++ frontend/crypto-hub/src/app/types/crypto.ts
@@ -5,5 +5,17 @@ export interface Crypto {
     description: string;
     imageUrl: string;
     createdAt?: Date;
+    comments: any[];
+    _id: string;
+}
+
+export interface CryptoDetails {
+    name: string;
+    symbol: string;
+    currentPrice: number;
+    description: string;
+    imageUrl: string;
+    createdAt?: Date;
+    comments: any[];
     _id: string;
 }
