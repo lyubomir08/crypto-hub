3f08588c89dd9f04352b48abe03fbb2295bf4139
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-01 16:06:49 +0200

    create search functionality

--- frontend/crypto-hub/src/app/cryptos/search-crypto/search-crypto.component.ts
+++ frontend/crypto-hub/src/app/cryptos/search-crypto/search-crypto.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ApiService } from '../../api.service';
+import { Crypto } from '../../types/crypto';
+import { CommonModule } from '@angular/common';
+import { LoaderComponent } from '../../shared/loader/loader.component';
 
 @Component({
-  selector: 'app-search-crypto',
-  standalone: true,
-  imports: [],
-  templateUrl: './search-crypto.component.html',
-  styleUrl: './search-crypto.component.css'
+    selector: 'app-search-crypto',
+    standalone: true,
+    imports: [FormsModule, CommonModule, LoaderComponent],
+    templateUrl: './search-crypto.component.html',
+    styleUrls: ['./search-crypto.component.css'],
 })
 export class SearchCryptoComponent {
+    cryptos: Crypto[] = [];
+    isLoading = false;
 
+    constructor(private apiService: ApiService) { }
+
+    search(form: NgForm) {
+        if(form.invalid) {
+            return;
+        }
+
+        const { name, symbol } = form.value;
+
+        this.isLoading = true;
+
+        this.apiService.searchCryptos(name, symbol).subscribe(
+            (data) => {
+                this.cryptos = data;
+                this.isLoading = false;
+            },
+        );
+    }
 }
