146e977968e8c281cdd7cd1bb95ecbb58cc27db2
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-01 14:35:50 +0200

    add lazy loading for create component

--- frontend/crypto-hub/src/app/app.routes.ts
+++ frontend/crypto-hub/src/app/app.routes.ts
@@ -9,16 +9,21 @@ import { AddCryptoComponent } from './cryptos/add-crypto/add-crypto.component';
 import { ErrorMsgComponent } from './core/error-msg/error-msg.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
-    { path: 'cryptos', children: [
-        {path: '', component: CatalogComponent},
-        {path: 'create', component: AddCryptoComponent}
-    ] },
-    {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent},
-    {path: 'error', component: ErrorMsgComponent},
-    {path: '404', component: PageNotFoundComponent},
-    {path: '**', redirectTo: '/404', pathMatch: 'full'},
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    {
+        path: 'cryptos', children: [
+            { path: '', component: CatalogComponent },
+            {
+                path: 'create', loadComponent: () =>
+                    import('./cryptos/add-crypto/add-crypto.component').then((c) => c.AddCryptoComponent),
+            },
+        ]
+    },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'error', component: ErrorMsgComponent },
+    { path: '404', component: PageNotFoundComponent },
+    { path: '**', redirectTo: '/404', pathMatch: 'full' },
 ];
