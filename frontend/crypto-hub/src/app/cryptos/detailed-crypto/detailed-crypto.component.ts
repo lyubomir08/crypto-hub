c5360a8a12d8769649c8e3bfd3b9977456f1fe0c
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-01 19:55:46 +0200

    add loader

--- frontend/crypto-hub/src/app/cryptos/detailed-crypto/detailed-crypto.component.ts
+++ frontend/crypto-hub/src/app/cryptos/detailed-crypto/detailed-crypto.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../api.service';
 import { ActivatedRoute } from '@angular/router';
 import { CryptoDetails } from '../../types/crypto';
+import { LoaderComponent } from '../../shared/loader/loader.component';
 
 @Component({
     selector: 'app-detailed-crypto',
     standalone: true,
-    imports: [],
+    imports: [LoaderComponent],
     templateUrl: './detailed-crypto.component.html',
     styleUrl: './detailed-crypto.component.css'
 })
 export class DetailedCryptoComponent implements OnInit {
     crypto: CryptoDetails | null = null;
+    isLoading: boolean = true;
 
     constructor(private apiService: ApiService, private route: ActivatedRoute) { }
 
@@ -20,6 +22,7 @@ export class DetailedCryptoComponent implements OnInit {
 
         this.apiService.getOneCrypto(id).subscribe((crypto: CryptoDetails) => {
             this.crypto = crypto;
+            this.isLoading = false;
         });
     }
 }
