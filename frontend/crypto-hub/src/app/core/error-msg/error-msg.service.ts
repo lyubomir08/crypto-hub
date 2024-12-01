1dbdd02084b7d15e6f3751fc7cffe0b7ab1cb891
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-01 14:15:55 +0200

    config setError method in service

--- frontend/crypto-hub/src/app/core/error-msg/error-msg.service.ts
+++ frontend/crypto-hub/src/app/core/error-msg/error-msg.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ErrorMsgService {
+    private apiError$$ = new BehaviorSubject(null);
+    public apiError$ = this.apiError$$.asObservable();
 
-  constructor() { }
+    constructor() { }
+
+    setError(error: any): void {
+        this.apiError$$.next(error);
+    }
 }
