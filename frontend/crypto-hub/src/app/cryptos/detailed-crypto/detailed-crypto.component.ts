11a8c5e547e772f0fb2b84674549a6f856361d3a
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-01 19:50:06 +0200

    add basic crypto visualization

--- frontend/crypto-hub/src/app/cryptos/detailed-crypto/detailed-crypto.component.ts
+++ frontend/crypto-hub/src/app/cryptos/detailed-crypto/detailed-crypto.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { ActivatedRoute } from '@angular/router';
+import { CryptoDetails } from '../../types/crypto';
 
 @Component({
-  selector: 'app-detailed-crypto',
-  standalone: true,
-  imports: [],
-  templateUrl: './detailed-crypto.component.html',
-  styleUrl: './detailed-crypto.component.css'
+    selector: 'app-detailed-crypto',
+    standalone: true,
+    imports: [],
+    templateUrl: './detailed-crypto.component.html',
+    styleUrl: './detailed-crypto.component.css'
 })
-export class DetailedCryptoComponent {
+export class DetailedCryptoComponent implements OnInit {
+    crypto: CryptoDetails | null = null;
 
+    constructor(private apiService: ApiService, private route: ActivatedRoute) { }
+
+    ngOnInit(): void {
+        const id = this.route.snapshot.params['cryptoId'];
+
+        this.apiService.getOneCrypto(id).subscribe((crypto: CryptoDetails) => {
+            this.crypto = crypto;
+        });
+    }
 }
