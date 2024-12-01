ec27a33ee88850618f03ec89c2d02516625961ba
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-01 14:07:36 +0200

    add dynamic error catching

--- frontend/crypto-hub/src/app/app.interceptor.ts
+++ frontend/crypto-hub/src/app/app.interceptor.ts
@@ -1,5 +1,9 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../environments/environment.development';
+import { catchError } from 'rxjs';
+import { inject } from '@angular/core';
+import { ErrorMsgService } from './core/error-msg/error-msg.service';
+import { Router } from '@angular/router';
 
 const { apiUrl } = environment;
 const API = '/api';
@@ -11,7 +15,20 @@ export const appInterceptor: HttpInterceptorFn = (req, next) => {
             withCredentials: true,
         })
     }
-    
 
-    return next(req);
+    const errorMsgService = inject(ErrorMsgService);
+    const router = inject(Router);
+
+    return next(req).pipe(
+        catchError((err) => {
+            if(err.status === 401) {
+                router.navigate(['/login']);
+            } else {
+                errorMsgService.setError(err);
+                router.navigate(['/error']);
+            }
+
+            return [err];
+        })
+    );
 };
