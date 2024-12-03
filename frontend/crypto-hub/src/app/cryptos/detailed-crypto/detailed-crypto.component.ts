0a9c1daf71f08d3d2c9ae75c2df79e1230fbaa60
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-03 09:54:38 +0200

    add isOwner func

--- frontend/crypto-hub/src/app/cryptos/detailed-crypto/detailed-crypto.component.ts
+++ frontend/crypto-hub/src/app/cryptos/detailed-crypto/detailed-crypto.component.ts
@@ -3,6 +3,8 @@ import { ApiService } from '../../api.service';
 import { ActivatedRoute } from '@angular/router';
 import { CryptoDetails } from '../../types/crypto';
 import { LoaderComponent } from '../../shared/loader/loader.component';
+import { UserForAuth } from '../../types/user';
+import { UserService } from '../../user/user.service';
 
 @Component({
     selector: 'app-detailed-crypto',
@@ -14,15 +16,24 @@ import { LoaderComponent } from '../../shared/loader/loader.component';
 export class DetailedCryptoComponent implements OnInit {
     crypto: CryptoDetails | null = null;
     isLoading: boolean = true;
+    currentUser: UserForAuth | null = null;
 
-    constructor(private apiService: ApiService, private route: ActivatedRoute) { }
+    constructor(private apiService: ApiService, private route: ActivatedRoute, private userService: UserService) { }
 
     ngOnInit(): void {
         const id = this.route.snapshot.params['cryptoId'];
+
+        this.userService.getProfile().subscribe((user) => {
+            this.currentUser = user;       
+        });
 
         this.apiService.getOneCrypto(id).subscribe((crypto: CryptoDetails) => {
             this.crypto = crypto;
             this.isLoading = false;
         });
+    }
+
+    isOwner(): boolean {
+        return this.currentUser?._id == this.crypto?.owner;
     }
 }
