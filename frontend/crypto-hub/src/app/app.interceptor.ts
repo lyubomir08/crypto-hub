a86088ff6b3cbca02745ac9011f2f7c8d759f536
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-03 11:27:07 +0200

    modify handling errors in interceptor

--- frontend/crypto-hub/src/app/app.interceptor.ts
+++ frontend/crypto-hub/src/app/app.interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../environments/environment.development';
-import { catchError } from 'rxjs';
+import { catchError, throwError } from 'rxjs';
 import { inject } from '@angular/core';
 import { ErrorMsgService } from './core/error-msg/error-msg.service';
 import { Router } from '@angular/router';
@@ -9,11 +9,11 @@ const { apiUrl } = environment;
 const API = '/api';
 
 export const appInterceptor: HttpInterceptorFn = (req, next) => {
-    if(req.url.startsWith(API)) {
+    if (req.url.startsWith(API)) {
         req = req.clone({
             url: req.url.replace(API, apiUrl),
             withCredentials: true,
-        })
+        });
     }
 
     const errorMsgService = inject(ErrorMsgService);
@@ -21,14 +21,19 @@ export const appInterceptor: HttpInterceptorFn = (req, next) => {
 
     return next(req).pipe(
         catchError((err) => {
-            if(err.status === 401) {
+            if (err.status === 401) {
+                if (req.url.endsWith('/users/profile')) {
+                    console.warn('Profile request unauthorized:', err.message);
+                    return throwError(() => err);
+                }
+
                 router.navigate(['/login']);
             } else {
                 errorMsgService.setError(err);
                 router.navigate(['/error']);
             }
 
-            return [err];
+            return throwError(() => err);
         })
     );
 };
