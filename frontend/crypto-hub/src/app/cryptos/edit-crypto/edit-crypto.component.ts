d37f2b4f77bc1ba21dd9245dd0274eb3df0a770b
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-04 12:08:31 +0200

    add basic edit logic

--- frontend/crypto-hub/src/app/cryptos/edit-crypto/edit-crypto.component.ts
+++ frontend/crypto-hub/src/app/cryptos/edit-crypto/edit-crypto.component.ts
@@ -1,18 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ApiService } from '../../api.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CryptoDetails } from '../../types/crypto';
+import { LoaderComponent } from '../../shared/loader/loader.component';
 
 @Component({
     selector: 'app-edit-crypto',
     standalone: true,
-    imports: [FormsModule],
+    imports: [FormsModule, LoaderComponent],
     templateUrl: './edit-crypto.component.html',
     styleUrl: './edit-crypto.component.css'
 })
 export class EditCryptoComponent implements OnInit {
+    @ViewChild('form') form: NgForm | undefined;
+    crypto: CryptoDetails | null = null;
+    isLoading: boolean = true;
+
+    constructor(private apiService: ApiService, private route: ActivatedRoute, private router: Router) { }
 
     ngOnInit(): void {
-        
+        const cryptoId = this.route.snapshot.params['cryptoId'];
+
+        this.apiService.getOneCrypto(cryptoId).subscribe((crypto) => {
+            this.crypto = crypto;
+            this.isLoading = false;
+        });
+
+        if (this.form) {
+            console.log(this.crypto);
+            
+            this.form.setValue({
+                name: this.crypto?.name,
+                symbol: this.crypto?.symbol,
+                currentPrice: this.crypto?.currentPrice,
+                description: this.crypto?.description,
+                imageUrl: this.crypto?.imageUrl
+            });
+        }
     }
 
-    editCrypto() { }
+    editCrypto() {
+        if (!this.crypto) {
+            return;
+        }
+        const { name, symbol, currentPrice, description, imageUrl } = this.crypto;
+        const id = this.crypto?._id;
+
+        this.apiService.updateCrypto(id as any, name, symbol, currentPrice, description, imageUrl).subscribe(() => {
+            this.router.navigate(['/cryptos']);
+        });
+
+    }
 }
