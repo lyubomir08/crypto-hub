5969fcf2f72a2657fcacb0ab55c8beddb4a27421
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-04 11:29:00 +0200

    init basic functions

--- frontend/crypto-hub/src/app/cryptos/edit-crypto/edit-crypto.component.ts
+++ frontend/crypto-hub/src/app/cryptos/edit-crypto/edit-crypto.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-edit-crypto',
-  standalone: true,
-  imports: [],
-  templateUrl: './edit-crypto.component.html',
-  styleUrl: './edit-crypto.component.css'
+    selector: 'app-edit-crypto',
+    standalone: true,
+    imports: [FormsModule],
+    templateUrl: './edit-crypto.component.html',
+    styleUrl: './edit-crypto.component.css'
 })
-export class EditCryptoComponent {
+export class EditCryptoComponent implements OnInit {
 
+    ngOnInit(): void {
+        
+    }
+
+    editCrypto() { }
 }
