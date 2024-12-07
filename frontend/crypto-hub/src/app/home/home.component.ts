4eb0fb06338c69e1dd47bdb6d57c1cadcc12c87c
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-07 17:34:00 +0200

    add latest 3 cryptos rendering

--- frontend/crypto-hub/src/app/home/home.component.ts
+++ frontend/crypto-hub/src/app/home/home.component.ts
@@ -1,39 +1,40 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Crypto } from '../types/crypto';
-import { LoaderComponent } from '../shared/loader/loader.component';
 import { RouterLink } from '@angular/router';
+import { LoaderComponent } from '../shared/loader/loader.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [LoaderComponent, RouterLink],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    standalone: true,
+    imports: [RouterLink, LoaderComponent, CommonModule],
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  lastThreeCryptos: Crypto[] = [];
-  isLoading: boolean = false;
-  errorMessage: string | null = null;
+    lastThreeCryptos: Crypto[] = [];
+    isLoadingLastThree: boolean = false;
+    errorMessageLastThree: string | null = null;
 
-  constructor(private apiService: ApiService) {}
+    constructor(private apiService: ApiService) { }
 
-  ngOnInit() {
-    this.fetchLastThreeCryptos();
-  }
+    ngOnInit() {
+        this.fetchLastThreeCryptos();
+    }
 
-  fetchLastThreeCryptos() {
-    this.isLoading = true;
-    this.apiService.getLastThreeCryptos().subscribe({
-      next: (data) => {
-        this.lastThreeCryptos = data;
-        this.isLoading = false;
-      },
-      error: (err) => {
-        console.error(err);
-        this.errorMessage = 'Failed to load cryptocurrencies.';
-        this.isLoading = false;
-      },
-    });
-  }
+    fetchLastThreeCryptos() {
+        this.isLoadingLastThree = true;
+        this.apiService.getLastThreeCryptos().subscribe({
+            next: (data) => {
+                this.lastThreeCryptos = data;
+                this.isLoadingLastThree = false;
+            },
+            error: (err) => {
+                console.error(err);
+                this.errorMessageLastThree = 'Failed to load the latest cryptocurrencies.';
+                this.isLoadingLastThree = false;
+            },
+        });
+    }
 }
