d89799b92d927636677c6e1fbd6eeaceda23cc17
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-07 17:06:34 +0200

    add latest 3 cryptos on the home

--- frontend/crypto-hub/src/app/home/home.component.ts
+++ frontend/crypto-hub/src/app/home/home.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Crypto } from '../types/crypto';
+import { LoaderComponent } from '../shared/loader/loader.component';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [LoaderComponent, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  lastThreeCryptos: Crypto[] = [];
+  isLoading: boolean = false;
+  errorMessage: string | null = null;
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.fetchLastThreeCryptos();
+  }
+
+  fetchLastThreeCryptos() {
+    this.isLoading = true;
+    this.apiService.getLastThreeCryptos().subscribe({
+      next: (data) => {
+        this.lastThreeCryptos = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.errorMessage = 'Failed to load cryptocurrencies.';
+        this.isLoading = false;
+      },
+    });
+  }
 }
