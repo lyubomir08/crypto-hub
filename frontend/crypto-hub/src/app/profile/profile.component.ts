2890eb1fcd03e9025611b29dd1be1a3abc1cd4d6
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-12-25 21:18:59 +0200

    implement profile component with user data loading and error handling

--- frontend/crypto-hub/src/app/profile/profile.component.ts
+++ frontend/crypto-hub/src/app/profile/profile.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '.././user/user.service';
+import { LoaderComponent } from '../shared/loader/loader.component';
+import { CommonModule } from '@angular/common';
+import { UserForAuth } from '.././types/user';
 
 @Component({
-  selector: 'app-profile',
-  standalone: true,
-  imports: [],
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+    selector: 'app-profile',
+    standalone: true,
+    templateUrl: './profile.component.html',
+    imports: [LoaderComponent, CommonModule],
+    styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+    user: UserForAuth | null = null;
+    isLoading: boolean = true;
+    errorMessage: string | null = null;
 
+    constructor(private userService: UserService) { }
+
+    ngOnInit(): void {
+        this.loadUserProfile();
+    }
+
+    private loadUserProfile(): void {
+        this.isLoading = true;
+        this.userService.getProfile().subscribe({
+            next: (user) => {
+                console.log(user);
+                
+                this.user = user;
+                this.isLoading = false;
+            },
+            error: (err) => {
+                this.errorMessage = err?.message || 'Failed to load profile.';
+                this.isLoading = false;
+            },
+        });
+    }
 }
