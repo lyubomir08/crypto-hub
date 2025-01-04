84b439cf3238afe70f122271ca3774147d5fba22
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-01-04 15:46:57 +0200

    add chat link to header and implement user profile retrieval on init

--- frontend/crypto-hub/src/app/core/header/header.component.ts
+++ frontend/crypto-hub/src/app/core/header/header.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../../user/user.service';
 import { CommonModule } from '@angular/common';
+import { UserForAuth } from '../../types/user';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [RouterLink, CommonModule],
+    imports: [CommonModule, RouterLink],
     templateUrl: './header.component.html',
-    styleUrl: './header.component.css'
+    styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+    currentUser: UserForAuth | null = null;
+
+    constructor(private userService: UserService, private router: Router) {}
+
+    ngOnInit(): void {
+        if (this.isLoggedIn) {
+            this.userService.getProfile().subscribe({
+                next: (user) => {
+                    console.log(user);
+                    
+                    this.currentUser = user;
+                },
+                error: (err) => {
+                    console.error('Error fetching current user:', err);
+                },
+            });
+        }
+    }
+
     get isLoggedIn(): boolean {
         return this.userService.isLogged;
     }
@@ -19,11 +39,13 @@ export class HeaderComponent {
         return this.userService.isAdmin();
     }
 
-    constructor(private userService: UserService, private router: Router) {}
-
-    logout() {
+    logout(): void {
         this.userService.logout().subscribe(() => {
             this.router.navigate(['/home']);
         });
+    }
+
+    redirectToProfile(): void {
+        this.router.navigate(['/profile']);
     }
 }
