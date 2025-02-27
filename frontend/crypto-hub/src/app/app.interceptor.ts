d5b734a3b00dd797aeb10eb22ab63bd9f0dc2250
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2025-02-27 23:23:16 +0200

    Enhance app interceptor to handle token expiration and redirect to login

--- frontend/crypto-hub/src/app/app.interceptor.ts
+++ frontend/crypto-hub/src/app/app.interceptor.ts
@@ -1,12 +1,17 @@
-
 import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../environments/environment.prod';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from './user/user.service';
 import { catchError, throwError } from 'rxjs';
 
 const { apiUrl } = environment;
 const API = '/api';
 
 export const appInterceptor: HttpInterceptorFn = (req, next) => {
+    const router = inject(Router);
+    const authService = inject(UserService);
+
     if (req.url.startsWith(API)) {
         req = req.clone({
             url: req.url.replace(API, apiUrl),
@@ -16,13 +21,22 @@ export const appInterceptor: HttpInterceptorFn = (req, next) => {
 
     return next(req).pipe(
         catchError((err) => {
-            const customError = {
-                message: err?.error?.message || 'An error occurred.',
-                status: err.status || 500,
-                url: req.url,
-            };
+            const errorMessage = err?.error?.message || 'An error occurred.';
+            const statusCode = err.status || 500;
 
-            return throwError(() => customError);
+            if (statusCode === 401 && errorMessage === 'Token has expired') {
+                console.warn('Token expired. Logging out user...');
+                
+                authService.logout().subscribe(() => {
+                    router.navigate(['/login']);
+                });
+            }
+
+            return throwError(() => ({
+                message: errorMessage,
+                status: statusCode,
+                url: req.url,
+            }));
         })
     );
 };
